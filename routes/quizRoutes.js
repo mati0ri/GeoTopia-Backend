97227dd64b0e2351f5d0d4861b301f196fa28b3b
mati0ri <mateo.iori223@gmail.com>
2023-05-19 17:43:28 +0200

    middlware enlevé

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getQuizzes, createQuiz, getQuiz, updateQuiz, deleteQuiz } = require("../controllers/quizController");
 const validateToken = require("../middleware/tokenValidationHandler");
 
-router.use(validateToken);
+//router.use(validateToken);
 router.route('/').get(getQuizzes).post(createQuiz);
 router.route('/:id').get(getQuiz).put(updateQuiz).delete(deleteQuiz);
 

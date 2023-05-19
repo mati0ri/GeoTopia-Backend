178c10c4000aa53818032fb4e5235d621557a648
mati0ri <mateo.iori223@gmail.com>
2023-05-19 17:22:51 +0200

    imports avant

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+
+//ajout test
+const questionRoutes = require("./routes/questionRoutes")
+const passageRoutes = require("./routes/passageRoutes")
+const userRoutes = require("./routes/userRoutes")
+const quizRoutes = require("./routes/quizRoutes")
+const enAttenteRoutes = require("./routes/enAttenteRoutes")
+//fin
+
 connectDb();
 const app = express();
 
@@ -12,11 +21,21 @@ const port = process.env.PORT || 3002;
 app.use(cors());
 
 app.use(express.json());
+
+/*
 app.use("/api/question", require("./routes/questionRoutes"));
 app.use("/api/passage", require("./routes/passageRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 app.use("/api/proposition", require("./routes/enAttenteRoutes"));
+*/
+
+app.use("/api/question", questionRoutes);
+app.use("/api/passage", passageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/api/proposition", enAttenteRoutes);
+
 
 
 app.get("/api/test", (req, res) => {

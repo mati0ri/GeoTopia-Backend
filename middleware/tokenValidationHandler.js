6b7e95751a40d93351deeacfe8f40586ebdeb5fd
mati0ri <mateo.iori223@gmail.com>
2023-05-19 19:09:32 +0200

    middlware mis a jour

--- middleware/tokenValidationHandler.js
+++ middleware/tokenValidationHandler.js
@@ -8,18 +8,21 @@ const validateToken = asyncHandler(async (req, res, next) => {
         token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
             if (err) {
-                res.status(401);
+                res.status(401).send("User is not authorized");
                 throw new Error("User is not authorized");
             }
             req.user = decoded.user;
         });
 
         if (!token) {
-            res.status(401)
-            throw new Error("token is missing");
+            res.status(401).send("Token is missing");
+            throw new Error("Token is missing");
         }
+    } else {
+        res.status(401).send("No Authorization header has been provided");
+        throw new Error("No Authorization header has been provided");
     }
-    next()
+    next();
 });
 
 module.exports = validateToken;
